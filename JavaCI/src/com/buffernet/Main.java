edcc8f118151cfdb66835ae5ce3cc37f351ab6f5
Joe Estes <joeestes86@gmail.com>
2013-06-19 13:38:04 -0700

    initial upload

---
+++ JavaCI/src/com/buffernet/Main.java
@@ -0,0 +1,20 @@
+package com.buffernet;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String message = "hi, Joe!";
+		printMessage(message);
+
+	}
+	
+	public static String printMessage(String message){
+		System.out.println(message);
+		
+		return message;
+	}
+
+}
